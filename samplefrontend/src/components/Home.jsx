4dcb03c75b24a5c20e6ea1a02f1a7ca7c36a60b8
manojrajgopal <manojraj15@yahoo.com>
2025-09-21 20:49:19 +0530

    Added samplefrontend as normal folder

---
+++ samplefrontend/src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to InfiniteWaveX</h1>
+      <p>Shaping Dreams with Timeless Wave</p>
+    </div>
+  );
+};
+
+export default Home;
