0107b9378129fcb9602ebf859d1d607eaf35331f
Manoj5621 <muruli5621@gmail.com>
2025-10-20 09:43:48 +0530

    cart count is located in it position

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -41,7 +41,7 @@ const Home = () => {
     },
     { 
       name: "MAN", 
-      image: "https://images.unsplash.com/photo-1617137968429-3c386e5a2b0c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80",
+      image: "https://images.pexels.com/photos/1639729/pexels-photo-1639729.jpeg",
       subcategories: ["Shirts", "Pants", "Suits", "Outerwear", "Accessories"]
     },
     { 
